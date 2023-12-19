60d1f6cda1730c89c336fc3682be05b4ecdb8362
muhammadumar0820 <salimjonovmuhammadumar372@gmail.com>
2023-12-19 15:57:38 +0300

    masalalar yechildi

---
+++ index.js
@@ -0,0 +1,311 @@
+//Birinchi masala
+//function max(num1, num2, num3){
+//    let max;
+//    if(num1>num2){
+//        max=num1;
+//    }else{
+//        max=num2;
+//    }
+//    if(num3>max){
+//        max=num3;
+//    }else{
+//        max=max;
+//    }
+//    console.log(max);
+//}
+//max(13, 14, 15);
+
+
+//Ikkinchi masala
+//let num = 32
+//switch(num) {
+//    case 10:
+//        console.log("O'n");
+//        break;
+//    case 11:
+//        console.log("O'n bir");
+//        break;
+//    case 12:
+//        console.log("O'n ikki");
+//        break;
+//    case 13:
+//        console.log("O'n uch");
+//        break;
+//    case 14:
+//        console.log("O'n to'rt");
+//        break;
+//    case 15:
+//        console.log("O'n besh");
+//        break;
+//    case 16:
+//        console.log("O'n olti");
+//        break;
+//    case 17:
+//       console.log("O'n yetti");
+//        break;
+//    case 18:
+//        console.log("O'n sakkiz");
+//        break;
+ //   case 19:
+//        console.log("O'n to'qqiz");
+//        break;
+//    case 20:
+//        console.log("Yigirma");
+//        break;
+//    case 21:
+//        console.log("Yigirma bir");
+//        break;
+//    case 22:
+//        console.log("Yigirma ikki");
+//        break;
+//    case 23:
+ //       console.log("Yigirma uch");
+ //       break;
+//    case 24:
+//        console.log("Yigirma to'rt");
+//        break;
+//    case 25:
+//        console.log("Yigirma besh");
+//        break;
+//    case 26:
+ //       console.log("Yigirma olti");
+//        break;
+//    case 27:
+ //       console.log("Yigirma yetti");
+  //      break;
+//    case 28:
+//        console.log("Yigirma sakkiz");
+//        break;
+//    case 29:
+//        console.log("Yigirma to'qqiz");
+//        break;
+//    case 30:
+//        console.log("O'ttiz");
+//        break;
+ //   case 31:
+//        console.log("O'ttiz bir");
+//        break;
+//    case 32:
+//        console.log("O'ttiz ikki");
+//        break;
+//    case 33:
+//        console.log("O'ttiz uch");
+//        break;
+//    case 34:
+//        console.log("O'ttiz to'rt");
+//        break;
+//    case 35:
+//            console.log("O'ttiz besh");
+//        break;
+//    case 36:
+//        console.log("O'ttiz olti");
+//        break;
+//    case 37:
+//        console.log("O'ttiz yetti");
+//        break;
+//    case 38:
+//        console.log("O'ttiz sakkiz");
+//        break;
+//    case 39:
+//        console.log("O'ttiz to'qqiz");
+//        break;
+//    case 40:
+//        console.log("Qirq");
+//        break;
+//    case 41:
+//        console.log("Qirq bir");
+//        break;
+//    case 42:
+//        console.log("Qirq ikki");
+//        break;
+//    case 43:
+//        console.log("Qirq uch");
+//        break;
+//    case 44:
+//        console.log("Qirq to'rt");
+//        break;
+//    case 45:
+//        console.log("Qirq besh");
+//        break;
+//    case 46:
+//        console.log("Qirq olti");
+//        break;
+//    case 47:
+//        console.log("Qirq yetti");
+//        break;
+//    case 48:
+//        console.log("Qirq sakkiz");
+//        break;
+//    case 49:
+//        console.log("Qirq to'qqiz");
+ //       break;
+//    case 50:
+//        console.log("Ellik");
+//        break;
+//    case 51:
+//        console.log("Ellik bir");
+//        break;
+//    case 52:
+//        console.log("Ellik ikki");
+//        break;
+//    case 53:
+//        console.log("Ellik uch");
+//        break;
+//    case 54:
+//        console.log("Ellik to'rt");
+//        break;
+//    case 55:
+//        console.log("Ellik besh");
+//        break;
+//    case 56:
+//        console.log("Ellik olti");
+//        break;
+//    case 57:
+//        console.log("Ellik yetti");
+//        break;
+//    case 58:
+//        console.log("Ellik sakkiz");
+//        break;
+//    case 59:
+//        console.log("Ellik to'qqiz");
+//        break;
+//    case 60:
+//        console.log("Oltmish");
+//        break;
+//    case 61:
+//        console.log("Oltmish bir");
+//        break;
+//    case 62:
+//        console.log("Oltmish ikki");
+//        break;
+//    case 63:
+//        console.log("Oltmish uch");
+//        break;
+//    case 64:
+//        console.log("Oltmish to'rt");
+//        break;
+//    case 65:
+//        console.log("Oltmish besh");
+//        break;
+//    case 66:
+//        console.log("Oltmish olti");
+//        break;
+//    case 67:
+//        console.log("Oltmish yetti");
+//        break;
+//    case 68:
+//        console.log("Oltmish sakkiz");
+//        break;
+//    case 69:
+//        console.log("Oltmish to'qqiz");
+//        break;
+//    case 70:
+//        console.log("Yetmish");
+//        break;
+//    case 71:
+//        console.log("Yetmish bir");
+//        break;
+//    case 72:
+//        console.log("Yetmish ikki");
+//        break;
+//    case 73:
+//        console.log("Yetmish uch");
+//        break;
+//    case 74:
+//        console.log("Yetmish to'rt");
+//        break;
+//    case 75:
+//        console.log("Yetmish besh");
+//        break;
+//    case 76:
+//        console.log("Yetmish olti");
+//        break;
+ //   case 77:
+//        console.log("Yetmish yetti");
+//        break;
+//    case 78:
+//        console.log("Yetmish sakkiz");
+ //       break;
+//    case 79:
+ //       console.log("Yetmish to'qqiz");
+//        break;
+//    case 80:
+//        console.log("Sakson");
+//        break;
+//    case 81:
+//        console.log("Sakson bir");
+//        break;
+//    case 82:
+//        console.log("Sakson ikki");
+//        break;
+//    case 83:
+//        console.log("Sakson uch");
+//        break;
+//    case 84:
+//        console.log("Sakson to'rt");
+//        break;
+//    case 85:
+//        console.log("Sakson besh");
+//        break;
+ //   case 86:
+ //       console.log("Sakson olti");
+//        break;
+//    case 87:
+ //       console.log("Sakson yetti");
+ //       break;
+//    case 88:
+//        console.log("Sakson sakkiz");
+//        break;
+//    case 89:
+//        console.log("Sakson to'qqiz");
+//        break;
+ //   case 90:
+ //       console.log("To'qson");
+ //       break;
+ //   case 91:
+ //       console.log("To'qson bir");
+ //       break;
+//    case 92:
+//        console.log("To'qson ikki");
+//        break;
+//    case 93:
+ //       console.log("To'qson uch");
+//        break;
+//    case 94:
+//        console.log("To'qson to'rt");
+//        break;
+//    case 95:
+//        console.log("To'qson besh");
+//        break;
+//    case 96:
+//        console.log("To'qson olti");
+//        break;
+//    case 97:
+//        console.log("To'qson yetti");
+//        break;
+//    case 98:
+//        console.log("To'qson sakkiz");
+//        break;
+//    case 99:
+//        console.log("To'qson to'qqiz");
+//        break;
+//    default:
+//        console.log("bu son ikki xonalai emas!")
+//        break;
+//}
+
+
+//Uchinchi masala
+//function odd(){
+//let n = 32;
+//let counter = 0;
+//for(let i = 1; i<=n; i++){
+//  if (i%2==0){
+//    counter++;
+//  }
+//}
+//}
+//console.log(odd());
+
+
+
